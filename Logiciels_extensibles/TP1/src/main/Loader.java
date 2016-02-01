fc93c3d5126481547a43e55cc083d72a011fda5d
Liciax <aboubouille@gmail.com>
2016-02-01 15:09:27 +0100

    refactoring

--- Logiciels_extensibles/TP1/src/main/Loader.java
+++ Logiciels_extensibles/TP1/src/main/Loader.java
@@ -14,33 +14,28 @@ public class Loader {
 	}
 	
 	public static void main(String[] args){
-		String fichier ="src/bean.txt";
 		
-		String ligne, nomClasse;
+		String ligne = "";
 		Object o = null;
 		Class cl = null;
-		String[] attribut = null;
+		String[] attribut = null; 
 		Class[] param = {String.class};
 		
-		//lecture du fichier texte	
+		
 		try{
-			InputStream ips = new FileInputStream(fichier); 
-			InputStreamReader ipsr = new InputStreamReader(ips);
-			BufferedReader br = new BufferedReader(ipsr);
+			//lecture du fichier texte	
+			BufferedReader br = new BufferedReader(new FileReader("src/bean.txt"));
 			
-			
-			
-			while ((ligne=br.readLine())!=null){
-				if(ligne.contains("class")) {
-					nomClasse = ligne.substring(6); //on aurait pu fair avec methode split
-					cl = Class.forName(nomClasse);
-					//System.out.println(cl); //affiche nom de la classe
-				} else {
-					attribut = ligne.split("=");
+			while ((ligne=br.readLine()) != null){
+				if(ligne.contains("class")){
+					cl = Class.forName(ligne.split(" = ")[1]);
+				}else{
+					attribut = ligne.split(" = ");
 					if(o == null){
 						o = cl.newInstance();
 						System.out.println("L'object " + o + " a les valeurs suivantes :");
 					}
+					
 					Method method = cl.getMethod("set"+attribut[0],param);
 					Object[] obs = {attribut[0]};
 					method.invoke(o, obs);
@@ -48,8 +43,7 @@ public class Loader {
 					System.out.println("\t -" + attribut[1]);
 				}
 			}
-			br.close(); 
-			
+			br.close(); 	
 		}		
 		catch (Exception e){
 			System.out.println(e.toString());
